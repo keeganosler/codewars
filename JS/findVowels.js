766f17c9e961efd1358846d30f9ad17cdb4991a5
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-09 11:39:31 -0600

    Create findVowels.js

---
+++ JS/findVowels.js
@@ -0,0 +1,12 @@
+function findVowels (str) {
+  var vowels = ['a','e','i','o','u','A','E','I','O','U']
+  var vowelCounter = 0
+  for (var i=0; i<str.length; i++) {
+    if (vowels.includes(str[i])){
+      vowelCounter = vowelCounter + 1
+    }
+  }
+  console.log(vowelCounter)
+}
+
+findVowels('keegan')
