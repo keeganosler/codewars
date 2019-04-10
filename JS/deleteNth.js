7a2b6fe5d1f6177b58c0c03b722f3ab7b22059a4
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-10 10:03:36 -0600

    Create deleteNth.js

---
+++ JS/deleteNth.js
@@ -0,0 +1,8 @@
+function deleteNth(arr, num) {
+  //sort the array
+  var sortedArr = arr.slice().sort()
+  console.log(sortedArr)
+  //check if the next element in the array is the same as the current element
+}
+
+deleteNth([1,2,3,4,3], 2)
