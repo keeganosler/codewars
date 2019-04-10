39752bab81f434a76e834ec627930552bcc70210
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-10 10:53:22 -0600

    Update deleteNth.js

--- JS/deleteNth.js
+++ JS/deleteNth.js
@@ -1,18 +1,21 @@
 function deleteNth(arr, num) {
-  var sortedArr = arr.slice().sort()
-  console.log(sortedArr)
   var resArr = []
-  for (var i=0; i<sortedArr.length-1; i++) {
-    var counter = 1
-    resArr.push(arr[i])
-    if (sortedArr[i+1] == sortedArr[i]) {
-      counter = counter + 1 
-      if (counter >= num) {
-        resArr.push(arr[i])
+  for (var i=0; i<arr.length; i++) {
+    if (!resArr.includes(arr[i])) {
+      resArr.push(arr[i])
+      console.log(resArr)
+    } else {
+      var counter = 0
+      for (var j=0; j<resArr.length; j++) {
+        if (resArr[j] == arr[i]) {
+          counter = counter + 1
+          if (counter < num) {
+            resArr.push(arr[i])
+          }
+        }
       }
     }
   }
-  console.log(resArr)
 }
 
 deleteNth([1,2,3,4,3,3], 2)
