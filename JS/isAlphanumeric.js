81ae1d733379bada8bb8c3b441dcf1a3b73bd271
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-16 11:16:19 -0600

    Create isAlphanumeric.js

---
+++ JS/isAlphanumeric.js
@@ -0,0 +1,22 @@
+var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+var nums = '0123456789'.split('')
+
+function isAlphanumeric (string) {
+  var alphaNumeric
+  if (string.length < 1) {
+    alphaNumeric = false
+  } else {
+    for (var i=0; i<string.length; i++) {
+      curr = string[i]
+      if(!(alphabet.includes(curr) || nums.includes(curr))){
+        alphaNumeric = false
+      }
+      else {
+        alphaNumeric = true
+      }
+    }
+  }
+  return alphaNumeric
+}
+
+console.log(isAlphanumeric("here1111"))
