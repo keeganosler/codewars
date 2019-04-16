dce109ac008799bb2266cab2084c660403dcdb9b
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-16 13:37:08 -0600

    Update partList.js

--- JS/partList.js
+++ JS/partList.js
@@ -1,15 +1,21 @@
 function partList(arr) {
-  var finalArr
+  var finalArr = []
   if(arr.length <= 1) {
     console.log("too short!")
   } else {
-    for (var i=1; i<arr.length; i++) {
-      var list1 = arr.slice(0,1).toString()
-      var list2 = arr.slice(1,arr.length).toString()
-      finalArr.push([list1,list2])
+    for (var i=0; i<arr.length; i++) {
+      for (var j=0; j<arr.length; j++) {
+        var list1 = arr.slice(i,j).toString()
+        var list2 = arr.slice(j,arr.length).toString()
+        if(list1!="" && list2!="") {
+          finalArr.push([list1,list2])
+        }
+        
+      }
     }
   }
   console.log(finalArr)
 }
 
 partList(['one','two','three'])
+partList(['one','two','three','four','five'])
