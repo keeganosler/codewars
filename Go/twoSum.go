916517bd06381abf93639db4c05f0c5a4807e89b
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-24 09:48:28 -0600

    Update twoSum.go

--- Go/twoSum.go
+++ Go/twoSum.go
@@ -3,17 +3,19 @@ package main
 import "fmt"
 
 func main() {
-    var arr1 []int {1,2,3,4,5}
-    var arr2 []int {1,3,5,2,4}
-    var target := 10
+    var arr1 = []int {1,2,3,4,5}
+    var arr2 = []int {1,3,5,2,4}
+    var target int
+    target = 10
     
     var indexes = twoSum(arr1, arr2, target)
+    fmt.Println(indexes)
 }
 
-func twoSum(arr1 []int, arr2 []int, num) []int {
+func twoSum(arr1 []int, arr2 []int, num int) []int {
     var arr []int
-    for i:=0; i<len(arr1); i++ {
-        for j:=0; j<len(arr2); j++ {
+    for i:=0; i<len(arr1)-1; i++ {
+        for j:=0; j<len(arr2)-1; j++ {
             if (arr1[i] + arr[j] == num){
                 arr = append(arr, i)
                 arr = append(arr, j)
