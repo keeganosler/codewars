1869f7b6029b5fa41c24fc7bc868e4b0370bc5a3
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-25 09:12:55 -0600

    Create convertToMilitary.go

---
+++ Go/convertToMilitary.go
@@ -0,0 +1,35 @@
+package main
+
+import s "strings"
+import "fmt"
+import "bytes"
+
+var alph = []string {"Alpha", "Bravo", "Charlie", "Delta",
+                    "Echo", "Foxtrot", "Golf", "Hotel",
+                    "India", "Juliet", "Kilo", "Lima",
+                    "Mike", "November", "Oscar", "Papa",
+                    "Quebec", "Romeo", "Sierra", "Tango",
+                    "Uniform", "Victor", "Whiskey", "Xray",
+                    "Yankee", "Zulu"}
+
+func main() {
+   fmt.Println(convertToMilitary("If you can read this"))
+}
+
+func convertToMilitary(str string) string {
+  var goodStr = s.Replace(str, " ", "", 4)
+  var buf bytes.Buffer
+  var res string
+  for i:=0; i<len(goodStr); i++ {
+    firstLetter := string(goodStr[i])
+    for j:=0; j<len(alph); j++ {
+      if (string(alph[j][0])==firstLetter) {
+        buf.WriteString(alph[j])
+        
+      }
+      
+    }
+    res = buf.String()
+  }
+  return res
+}
