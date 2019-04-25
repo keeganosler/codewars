eb5377a608880972693f3b08496b9ba088adfd85
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-25 13:55:26 -0600

    Update isTrianglePossible.go

--- Go/isTrianglePossible.go
+++ Go/isTrianglePossible.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
-	"math"
+
 )
 
 func main() {
-	
+	fmt.Println(isTrianglePossible(3,4,5))
+	fmt.Println(isTrianglePossible(10,4,5))
 }
 
 func isTrianglePossible (a int, b int, c int) bool {
-
+	var isPossible bool
+	var ab = a + b
+	var ac = a + c
+	var bc = b + c
+	if (ab > c && ac > b && bc > a) {
+		isPossible = true
+	} else {
+		isPossible = false
+	}
+	return isPossible
 }
+
