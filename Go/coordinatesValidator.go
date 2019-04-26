287b9d61204f85c29a34788db7689a2621812510
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-26 09:03:31 -0600

    finish program with first test case

--- Go/coordinatesValidator.go
+++ Go/coordinatesValidator.go
@@ -5,11 +5,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, playground")
+	var testArr1 = []float64 {51.234, 122.345}
+	fmt.Println(coordValidator(testArr1))
 }
 
 func coordValidator(arr []float64) bool {
-	for i:=0; i<len(arr); i++ {
-		
+	if (arr[0] <= 90.0 && arr[0] >= 0.0 && arr[1] <=180.0 && arr[1] >= 0.0) {
+		return true
+	} else {
+		return false
 	}
 }
