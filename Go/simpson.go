e2bed14ed94ddc5f6d31dce8b27c3c0a7e80a0b2
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-29 09:25:18 -0600

    Update simpson.go

--- Go/simpson.go
+++ Go/simpson.go
@@ -6,15 +6,37 @@ import(
     )
 
 func main() {
-   fmt.Println(simpson(2))
+   fmt.Println(simpson(4))
 }
 
-func simpson(n) float64 {
-    var a = 0
+func simpson(n int) float64 {
+    var a = 0.0
     var b = math.Pi
-    var co = (b - a)/(3*n)
+    var h = (b-a)/float64(n)
+    var co = (b - a)/float64(3*n)
     var t1 = createFunc(a) + createFunc(b)
-    var sum1 = createSum1()
+    var sum1 = 4*createSum1(a,n,h)
+    var sum2 = 2*createSum2(a,n,h)
+    var res = co*(t1 + sum1 + sum2)
+    return res
+}
+
+func createSum1(a float64, n int, h float64) float64 {
+    var res = 0
+    for i:=0; i<n/2; i++ {
+        var f = a + ((2*float64(i))-1)*h
+        res = res + createFunc(f)
+    }
+    return res
+}
+
+func createSum2(a float64, n int, h float64) float64 {
+    var res = 0
+    for i:=0; i<n/2 -1; i++ {
+        var f = a + 2*i*h
+        res = res + createFunc(f)
+    }
+    return res
 }
 
 func createFunc(i float64) float64 {
