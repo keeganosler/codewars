dbd852e443d0a0b5095613bbb67291eea43c4c92
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-29 09:34:53 -0600

    Create fusc.go

---
+++ Go/fusc.go
@@ -0,0 +1,25 @@
+package main
+
+import(
+    "fmt"
+    )
+
+func main() {
+   fmt.Println(fusc(10))
+}
+
+func fusc(n int) int {
+    var res int
+    if(n == 0) {
+        res = 0
+    } else if(n == 1) {
+        res = 1
+    } else if(n%2 == 0) {
+        res = fusc(n/2)
+    } else if((n-1)%2 == 0) {
+        res = fusc(n/2) + fusc(n/2 + 1)
+    } else {
+        res = 0
+    }
+    return res
+}
