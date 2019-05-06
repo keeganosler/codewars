12e4c67a739b224bd3d6921415f744561d8069fa
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-05-06 09:00:19 -0600

    Update maxAbs.go

--- Go/maxAbs.go
+++ Go/maxAbs.go
@@ -3,18 +3,31 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println(maxAbs("", ""))
+  var a1 = []string {}
+  var a2 = []string {}
+  fmt.Println(maxAbs(a1, a2))
 }
 
-func maxAbs(str1 []string, str2 []string) string {
-  var res string
-  max maxSub int
+func maxAbs(str1 []string, str2 []string) int {
+  var res int
+  var sub int
+  var maxSub int
   if(len(str1) == 0 || len(str2) == 0) {
-    res = "Nothing"
+    res = -1
   } else {
     for i:=0; i<len(str1); i++ {
-      
+      for j:=0; j<len(str2); j++ {
+        if len(str1[i]) > len(str2[j]) {
+          sub = len(str1[i]) - len(str2[j])
+        } else {
+          sub = len(str2[j]) - len(str1[i])
+        }
+        if maxSub > sub {
+          maxSub = sub
+        }
+      }
     }
+    res = maxSub
   }
   return res
 }
