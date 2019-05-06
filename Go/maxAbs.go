d0ddeec7635eeaa3f0d7aa96b3291a0e80d19108
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-05-06 08:54:54 -0600

    Create maxAbs.go

---
+++ Go/maxAbs.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println(maxAbs("", ""))
+}
+
+func maxAbs(str1 []string, str2 []string) string {
+  var res string
+  max maxSub int
+  if(len(str1) == 0 || len(str2) == 0) {
+    res = "Nothing"
+  } else {
+    for i:=0; i<len(str1); i++ {
+      
+    }
+  }
+  return res
+}
