2241c8443b9bb6ead89aea11a3b7f754ef4a8b6b
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-05-15 08:43:40 -0600

    Update easyWallpaper.go

--- Go/easyWallpaper.go
+++ Go/easyWallpaper.go
@@ -1,11 +1,21 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math"
+    )
 
 func main() {
-   fmt.Printf(easyWallpaper(l,w,h))
+   l := 4.0
+   w := 3.5
+   h := 3.0
+   fmt.Println(easyWallpaper(l,w,h))
 }
 
-func easyWallpaper(l, w, h float64) string {
-    
+func easyWallpaper(l, w, h float64) float64 {
+ walls := 2.0*l*h + 2.0*w*h
+ roll := 0.52*10
+ paper := walls/roll
+ paper = paper + 0.15*paper
+ return math.Ceil(paper)
 }
