3c074e60aff89f0d3c123519cd277972ea534761
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-05-27 09:25:29 -0600

    Update evennessDifference.js

--- JS/evennessDifference.js
+++ JS/evennessDifference.js
@@ -1,11 +1,27 @@
+console.log(evennessDifference([2,4,7,8,10]))
+
 function evennessDifference(arr) {
+  var evenCount, oddCount = 0
   var index
-  for (var i=0; i<arr.length; i++) {
-    if(arr[i]%2 != 0) {
-      index = i + 1
+  for(var i=0; i<arr.length; i++) {
+    if(arr[i]%2 == 0) {
+      evenCount = evenCount + 1
+    } else {
+      oddCount = oddCount + 1
+    }
+  }
+  if(evenCount > oddCount) {
+    for(var i=0; i<arr.length; i++) {
+      if(arr[i]%2 != 0) {
+        index = i + 1
+      }
+    }
+  } else {
+    for(var i=0; i<arr.length; i++) {
+      if(arr[i]%2 == 0) {
+        index = i + 1
+      }
     }
   }
   return index
 }
-
-console.log(evennessDifference([2,4,7,8,10]))
