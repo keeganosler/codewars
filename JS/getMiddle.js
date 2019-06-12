82507d8a32e0d9baccc0e278de6dbcad88ff6948
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-06-12 09:47:08 -0600

    Update getMiddle.js

--- JS/getMiddle.js
+++ JS/getMiddle.js
@@ -1,7 +1,16 @@
 function getMiddle(str) {
+  var subStr
   if(str.length%2 == 0) {
-    
+    var mid = str.length / 2 
+    subStr = str.slice(mid-1, mid+1)
   } else {
-    
+      var mid = str.length / 2
+      var lower = Math.floor(mid)
+      var upper = Math.ceil(mid)
+      subStr = str.slice(lower, upper)    
   }
+  return subStr
 }
+
+console.log(getMiddle("erin"))
+console.log(getMiddle("erino"))
