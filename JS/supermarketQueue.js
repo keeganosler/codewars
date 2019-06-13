b539b4292d35f2e7dfc2acad3719cc2f68f54f12
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-06-13 10:08:55 -0600

    Update supermarketQueue.js

--- JS/supermarketQueue.js
+++ JS/supermarketQueue.js
@@ -1,1 +1,17 @@
+function supermarketQueue(arr, n) {
+  var totalTime
+  if(n == 1) {
+    totalTime = sumArr(arr)
+  }
+  return totalTime
+}
 
+function sumArr(arr) {
+  var sum = 0
+  for(var i=0; i<arr.length; i++) {
+    sum = sum + arr[i]
+  }
+  return sum
+}
+
+console.log(supermarketQueue([10,3,3,2], 1))
