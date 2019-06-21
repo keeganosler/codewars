979f688df26fc321ac7a5ca97581f9bf6ed8927f
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-06-21 09:37:12 -0600

    Create count_smileys.py

---
+++ Python/count_smileys.py
@@ -0,0 +1,19 @@
+def count_smileys(arr):
+    count = 0
+    for x in arr:
+        is_eyes = False
+        is_nose = False
+        is_mouth = False
+        if(x[0] == ':' or x[0] == ';'):
+            is_eyes = True
+        if(x[1] == '-' or x[1] == '~'):
+            is_nose = True
+        if(x[2] == ')' or x[2] == 'D'):
+            is_mouth = True
+        if(is_eyes == True and is_nose == True and is_mouth == True):
+            count = count + 1 
+    print(count)
+    
+
+count_smileys([':-)', ';-D'])
+count_smileys([';-D', ':-(', ':-)', ';~)'])
