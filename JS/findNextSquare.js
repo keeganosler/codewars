fe40142e43f35807c9af68b5cd9b255bd242fd8a
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-07-02 09:12:11 -0600

    Create findNextSquare.js

---
+++ JS/findNextSquare.js
@@ -0,0 +1,15 @@
+function findNextSquare(sq) {
+  var res
+  var n = Math.sqrt(sq)
+  if(Number.isInteger(n)) {
+    var next = n + 1 
+    res = next*next
+  } else {
+    res = -1
+  }
+  return res
+}
+
+console.log(findNextSquare(121))
+console.log(findNextSquare(625))
+console.log(findNextSquare(114))
