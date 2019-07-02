0c26ed91ef9a797530e8cd0ed1f453a7086da517
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-07-02 10:25:52 -0600

    Create sortAndTransform.js

---
+++ JS/sortAndTransform.js
@@ -0,0 +1,16 @@
+function sortAndTransform(arr){
+  var ascArr = arr.sort(function(a,b){return a-b})
+  var desArr = arr.sort(function(a,b){return b-a})
+  var alphArr = convertToAscii(arr)
+}
+
+function convertToAscii(arr) {
+  var asciiArr = []
+  for(var i=0; i<arr.length; i++) {
+    var a = String.fromCharCode(i)
+    asciiArr.push(a)
+  }
+  return asciiArr
+}
+
+console.log(convertToAscii([65, 66, 67]))
