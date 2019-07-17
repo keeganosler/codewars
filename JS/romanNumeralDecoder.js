9c9e7efe496e78068c0b19ffb332135b6cf44934
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-07-17 08:47:31 -0600

    Create romanNumeralDecoder.js

---
+++ JS/romanNumeralDecoder.js
@@ -0,0 +1,16 @@
+var numerals = ['I','V','X','L','C','D','M']
+var nums = [1,5,10,50,100,500,1000]
+
+function romanNumeralDecoder(str) {
+  var sum = 0
+  for(var i=0; i<str.length; i++) {
+    if(!numerals.includes(str[i])){
+      console.log('not a roman numeral!')
+    } else {
+      var sum = sum + nums[numerals.indexOf(str[i])]
+    }
+  }
+  return sum
+}
+
+console.log(romanNumeralDecoder('XXI'))
