83ebe662106aba6e8c26211bd3981df4acf82cd6
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-07-19 13:19:37 -0600

    Create reverseEachWordInString.js

---
+++ JS/reverseEachWordInString.js
@@ -0,0 +1,18 @@
+function reverseEachWord(str){
+  var words = str.split(" ")
+  var wordsReversed = []
+  for(var i =0; i<words.length; i++) {
+    wordsReversed.push(reverseString(words[i]))
+  } 
+  var reversedWords = wordsReversed.join(" ")
+  return reversedWords
+}
+
+function reverseString(str) {
+  var split = str.split("")
+  var revArr = split.reverse()
+  var arr = revArr.join("")
+  return arr
+}
+
+console.log(reverseEachWord("hello there"))
