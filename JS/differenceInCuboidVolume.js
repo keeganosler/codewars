2c2bf5c2d12ac31157aaef99d041898ff3f34437
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-08-08 09:36:26 -0600

    Update differenceInCuboidVolume.js

--- JS/differenceInCuboidVolume.js
+++ JS/differenceInCuboidVolume.js
@@ -1,9 +1,5 @@
-var a = [3,4,5]
-var b = [6,7,8]
-
 function differenceInCuboidVolume(arr1,arr2) {
   var a1, a2 = 1
-  var res
   for(var i=0; i<arr1.length; i++) {
     a1 = a1*arr1[i]
   }
@@ -11,13 +7,12 @@ function differenceInCuboidVolume(arr1,arr2) {
     a2 = a2*arr2[i]
   }
   if(a1 === a2) {
-    res = 0
+    return 0
   } else if (a1 >= a2){
-    res = a1 - a2
+    return a1 - a2
   } else {
-    res = a2 - a1
+    return a2 - a1
   }
-  return res
 }
 
-console.log(differenceInCuboidVolume(a,b))
+console.log(differenceInCuboidVolume([3,4,5],[6,7,8]))
