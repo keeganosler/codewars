a395669f3ad227b95e8cf21feeb70e4745c1dbaf
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-08-28 08:43:58 -0600

    Create authenticate.py

---
+++ Python/authenticate.py
@@ -0,0 +1,8 @@
+def authenticate():
+    name = input("Please enter your name: ")
+    password = input("Please enter your password: ")
+    if(name == "Santa Claus" and password == "hohoho"):
+        print("Authenticated!")
+        
+    
+authenticate()
