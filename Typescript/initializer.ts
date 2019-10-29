f634d84adba2e86447ca250511b3ddd0e1c4d863
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-10-29 11:58:46 -0600

    Create initializer.ts

---
+++ Typescript/initializer.ts
@@ -0,0 +1,12 @@
+function initializer(name: string) {
+    var initials: string
+    var idx = name.indexOf(' ')
+    if (idx == -1) {
+        initials = 'Not a valid name'
+    } else { 
+        initials = name[0] + '.' + name[idx+1]
+    }
+    return initials
+}
+
+ console.log(initializer('Keegan Osler'))
