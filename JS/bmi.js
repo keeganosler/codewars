efd43de348466f0e94172663039cd46a547e091b
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-11-21 17:18:57 -0700

    Create bmi.js

---
+++ JS/bmi.js
@@ -0,0 +1,17 @@
+function bmi(height, weight) {
+  var bmi 
+  var res = weight / height
+  res = res * res
+  if(res <= 18.5) {
+    bmi = "Underweight"
+  } else if (res > 18.5 && res <= 25) {
+    bmi = "Normal Weight"
+  } else if (res > 25 && res <= 30) {
+    bmi = "Overweight"
+  } else {
+    bmi = "Obese"
+  }
+  return bmi 
+}
+
+console.log(bmi(67, 150))
