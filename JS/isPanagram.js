13fcb700a1cce4c69785912ab30595f06cec9eb5
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-11-22 16:26:17 -0700

    Update isPanagram.js

--- JS/isPanagram.js
+++ JS/isPanagram.js
@@ -1,5 +1,6 @@
-var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+/* determines if a given string is a panagram */
 
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 function isPanagram (str) {
   var isCount = 0
