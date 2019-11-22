a0a4c013b257842a3683f35cff41b16e4e9d2bdf
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-11-22 10:08:13 -0700

    Update FindShortestWord.go

--- Go/FindShortestWord.go
+++ Go/FindShortestWord.go
@@ -1,6 +1,8 @@
 package kata
 import "strings"
 
+/* finds the shortest words in a set of stirngs */
+
 func FindShortestWord (s string) int {
 	shortestWord := len(s)
 	for _, word := range strings.Split(s, " ") {
